80841c7ed3f4a67e0db5ed41a7858f1f9494989e
Matthew Bates <BatesMatthewJ@GMail.com>
2015-04-10 14:37:33 -0400

    Router tick completed with placeholder helper methods

--- src/Router.java
+++ src/Router.java
@@ -1,8 +1,86 @@
 
 public class Router extends NetworkDevice{
 
+	boolean hasTableChanged;
+
 	public void tick() {
-		// TODO	
+		if(hasTableChanged){
+			RoutingPacket routingPacket = getRoutingPacket();
+			
+			for(Link outLink : outLinks){
+				if(!outLink.isBusy()){
+					outLink.addPacket(routingPacket);
+				}
+			}
+		
+		}
+		
+		for(Link sourceLink : inLinks){
+			Packet toSend = sourceLink.peekHead();
+			
+			if(toSend instanceof RoutingPacket){
+				updateRoutingTable((RoutingPacket)toSend);
+				
+			}else{
+				Link destLink = getDestinationLink(toSend);
+				
+				if(destLink == null){
+					//Drop packet if we can't forward it
+					sourceLink.popHead();
+				}else{
+					if(!destLink.isBusy()){
+						//if outbound link isn't busy forward the packet
+						destLink.addPacket(toSend);
+						sourceLink.popHead();
+					}else{
+						//if the outbound link is busy, just move on to the next input link
+						//do nothing
+					}
+				}
+			}
+		}
+	}
+	
+	public RoutingPacket getRoutingPacket(){
+		//TODO
+		//create and return a meaningful routing packet. maybe just a copy of the routing table?
+		return null;
+	}
+	
+	public void updateRoutingTable(RoutingPacket packet){
+		//TODO
+		//read packet and update our routing table appropriately
+		//if it changed, set hasTableChanged to true, which will trigger new routing packets to send in next tick
+	}
+	
+	public Link getDestinationLink(Packet packet){
+		//TODO
+		//use routing table to determine where packet should go, return outbound link
+		return null;
+	}
+	
+	public void addInLink(Link link){
+		super.addInLink(link);
+		//routing table will update when routingpacket is received
+		//do not need to do anything else here
+	}
+	
+	public void addOutLink(Link link){
+		super.addOutLink(link);
+		hasTableChanged = true;
+		//TODO
+		//Update routing table and set hasTableChanged to true if it has changed
+	}
+	
+	public boolean removeLink(Link link){
+		boolean value = super.removeLink(link);
+
+		hasTableChanged = true;
+		
+		//TODO
+		//Update routing table and set hasTableChanged to true if it has changed
+		
+		return value;
 	}
 	
 }
